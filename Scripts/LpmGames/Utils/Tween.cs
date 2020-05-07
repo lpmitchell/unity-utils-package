164b794ea0b345af13c27297df38df38fa242269
Luke Mitchell <luke@lpmitchell.com>
2020-05-07 10:56:14 +0100

    Include meta files, code styling update in Tween class

--- Scripts/LpmGames/Utils/Tween.cs
+++ Scripts/LpmGames/Utils/Tween.cs
@@ -30,32 +30,32 @@ namespace LpmGames.Utils
         
         public static IEnumerator Start(float duration, float start, float end, Action<float> onUpdate, Action onComplete, AnimationCurve curve = null)
         {
-            yield return Start(duration, t => { onUpdate(Mathf.LerpUnclamped(start, end, t)); }, onComplete, curve);
+            yield return Start(duration, t => onUpdate(Mathf.LerpUnclamped(start, end, t)), onComplete, curve);
         }
         
         public static IEnumerator Start(float duration, Color start, Color end, Action<Color> onUpdate, Action onComplete, AnimationCurve curve = null)
         {
-            yield return Start(duration, t => { onUpdate(Color.LerpUnclamped(start, end, t)); }, onComplete, curve);
+            yield return Start(duration, t => onUpdate(Color.LerpUnclamped(start, end, t)), onComplete, curve);
         }
         
         public static IEnumerator Start(float duration, Vector2 start, Vector2 end, Action<Vector2> onUpdate, Action onComplete, AnimationCurve curve = null)
         {
-            yield return Start(duration, t => { onUpdate(Vector2.LerpUnclamped(start, end, t)); }, onComplete, curve);
+            yield return Start(duration, t => onUpdate(Vector2.LerpUnclamped(start, end, t)), onComplete, curve);
         }
         
         public static IEnumerator Start(float duration, Vector3 start, Vector3 end, Action<Vector3> onUpdate, Action onComplete, AnimationCurve curve = null)
         {
-            yield return Start(duration, t => { onUpdate(Vector3.LerpUnclamped(start, end, t)); }, onComplete, curve);
+            yield return Start(duration, t => onUpdate(Vector3.LerpUnclamped(start, end, t)), onComplete, curve);
         }
         
         public static IEnumerator Start(float duration, Vector4 start, Vector4 end, Action<Vector4> onUpdate, Action onComplete, AnimationCurve curve = null)
         {
-            yield return Start(duration, t => { onUpdate(Vector4.LerpUnclamped(start, end, t)); }, onComplete, curve);
+            yield return Start(duration, t => onUpdate(Vector4.LerpUnclamped(start, end, t)), onComplete, curve);
         }
         
         public static IEnumerator Start(float duration, Quaternion start, Quaternion end, Action<Quaternion> onUpdate, Action onComplete, AnimationCurve curve = null)
         {
-            yield return Start(duration, t => { onUpdate(Quaternion.LerpUnclamped(start, end, t)); }, onComplete, curve);
+            yield return Start(duration, t => onUpdate(Quaternion.LerpUnclamped(start, end, t)), onComplete, curve);
         }
     }
 }
